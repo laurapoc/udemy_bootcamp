e75d0a9785e590b841a3514135d2de001a578732
laurapoc <laura.pociuniene@gmail.com>
2020-01-31 16:09:31 +0200

    JS for loop exercise added

--- JavaScript/while_loops_problem_set_exercise/script.js
+++ JavaScript/while_loops_problem_set_exercise/script.js
@@ -7,6 +7,12 @@ while (x <= 19) {
   x++;
 }
 
+// 1. Print all numbers between -10 and 19 FOR LOOP:
+console.log("Print all numbers between -10 and 19 FOR LOOP");
+for(let i = -10; i < 20; i++) {
+  console.log(i);
+}
+
 // 2. Print all even numbers between 10 and 40
 console.log("Print all even numbers between 10 and 40");
 let evenNumbers = 10;
@@ -14,6 +20,12 @@ let evenNumbers = 10;
 while (evenNumbers <= 40) {
   console.log(evenNumbers);
   evenNumbers += 2;
+}
+
+// Print all even numbers between 10 and 40 FOR LOOP:
+console.log("Print all even numbers between 10 and 40 FOR LOOP");
+for(let i = 10; i < 41; i += 2) {
+  console.log(i);
 }
 
 // 2. Print all even numbers between 10 and 40 (with module)
@@ -27,6 +39,14 @@ while (evenNumbersMod <= 40) {
   evenNumbersMod++;
 }
 
+// Print all even numbers between 10 and 40 (with module) FOR LOOP:
+console.log("Print all even numbers between 10 and 40 (with module) FOR LOOP");
+for(let i = 10; i < 41; i++) {
+  if(i % 2 === 0) {
+    console.log(i);
+  }
+}
+
 // 3. Print all odd numbers between 300 and 333
 console.log("Print all odd numbers between 300 and 333");
 let oddNum = 300;
@@ -38,6 +58,15 @@ while (oddNum <= 333) {
   oddNum++;
 }
 
+// 3. Print all odd numbers between 300 and 333 FOR LOOP:
+console.log("Print all odd numbers between 300 and 333 FOR LOOP:");
+for(let i = 300; i < 334; i++) {
+  if(i % 2 === 1) {
+    console.log(i);
+  }
+}
+
+
 // 4. print all numbers divisible by 5 AND 3 between 5 and 50
 console.log("print all numbers divisible by 5 AND 3 between 5 and 50");
 let divisibleNum = 5;
@@ -47,4 +76,13 @@ while (divisibleNum <= 50) {
     console.log(divisibleNum);
   }
   divisibleNum++;
+}
+
+// 4. print all numbers divisible by 5 AND 3 between 5 and 50 FOR LOOP:
+console.log("Print all numbers divisible by 5 AND 3 between 5 and 50 FOR LOOP");
+
+for(let i = 5; i < 51; i++) {
+  if(i % 5 === 0 && i % 3 === 0) {
+    console.log(i);
+  }
 }
